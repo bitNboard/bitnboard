44d9ef896aeaf69eeee04a3942d8e78d7c8b7b75
gauravtiwari <gaurav.tiwari@mai.io>
2025-02-05 23:59:32 +0530

    change about & team

--- src/components/about.jsx
+++ src/components/about.jsx
@@ -11,11 +11,11 @@ const About = () => {
     <div className="h-full min-h-screen flex flex-col items-center bg-white bg-grid font-primary justify-between">
       <div className="text-[3.5rem] w-full flex justify-center text-center mt-7">
         <div className="w-4/5 text-center">
-          <div className="font-bold">Team</div>
-          <p className="text-base font-medium">
-            We are Web3, AI, and robotics experts, contributing to top
+          <div className="font-bold">We are Web3, AI, and robotics experts, contributing to top
             open-source projects that power decentralized networks and
-            intelligent systems globally.
+            intelligent systems globally.</div>
+          <p className="text-base font-medium">
+            
           </p>
         </div>
       </div>
